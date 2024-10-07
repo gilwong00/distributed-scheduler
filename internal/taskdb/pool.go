8b9890b0d62accfbd2822c51c8673f2c5a632caf
Gilbert <gilwong3105@gmail.com>
2024-10-07 01:22:25 -0700

    fixup! Initial commit

--- internal/taskdb/pool.go
+++ internal/taskdb/pool.go
@@ -18,17 +18,16 @@ type PoolOptions func(*pgxpool.Config)
 
 // NewDB returns a *sql.DB
 //
-// This function will set the max number of idle connections to 0 (connections must be managed by pgxpool)
-// Do not alter the max number of idle connections on the returned db.
-// This is only intered for code or workflows that requires *sql.DB
+// This function will set the max number of idle connections to 0
+// since connections should be managed by pgxpool.
 func (c *Connection) NewDB() *sql.DB {
 	db := stdlib.OpenDBFromPool(c.Pool)
 	db.SetMaxOpenConns(int(c.Pool.Config().MaxConns))
 	return db
 }
 
-// NewPoolConnection returns a new Connection. This will be used for the main database connection pool
-// also safe to use concurrently.
+// NewPoolConnection returns a new Connection. This is the main database connection
+// and is also safe for concurrent connections.
 func NewPoolConnection(ctx context.Context, host string, port uint16, options ...PoolOptions) (*Connection, error) {
 	config, err := pgxpool.ParseConfig(
 		fmt.Sprintf("postgres://%s:%d", host, port),
